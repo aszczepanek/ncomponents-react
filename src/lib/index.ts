2658ccfdbac94139d806703119f0f621a37e7b48
Aleksander Szczepanek <aleksander.szczepanek@gmail.com>
2019-01-22 12:11:55 +0100

    Lib index.ts update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export { DatepickerInput } from './components/DatepickerInput';
+export { DatepickerView } from './components/DatepickerView';
 export { Multiselect } from './components/Multiselect';
 export { SelectInput } from './components/SelectInput';
+export { Dropdown } from './components/Dropdown';
